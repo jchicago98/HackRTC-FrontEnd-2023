6693c3b1a2a43c8c76f368214d7baacd663c46c0
Jorge Morin <jmorin1@hawk.iit.edu>
2023-10-02 17:29:56 -0500

    Modified UI to accept multiple call logs

---
+++ script.js
@@ -0,0 +1,109 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const chatMessages = document.getElementById("chatMessages");
+    const messageInput = document.getElementById("messageInput");
+    const sendMessageBtn = document.getElementById("sendMessageBtn");
+    const chatList = document.getElementById("chatList");
+  
+    let activeChat = null;
+    const chatHistory = {}; // Store chat history for each phone number
+  
+    // Create a WebSocket connection
+    const socket = new WebSocket("ws://localhost:443/");
+  
+    // Listen for WebSocket messages
+    socket.addEventListener("message", (event) => {
+      const message = JSON.parse(event.data);
+      //console.log(message);
+  
+      if (message.event === "messageSent" && message.message.from) {
+        const phoneNumber = message.message.to; // Use the "to" field for conversation identifier
+        if (!chatHistory[phoneNumber]) {
+          chatHistory[phoneNumber] = [];
+        }
+        chatHistory[phoneNumber].push(message);
+        if (activeChat === phoneNumber) {
+          displayChatHistory(phoneNumber);
+        }
+        updateChatList();
+      } else if (message.user === "user" && message.from) {
+        const phoneNumber = message.from;
+        if (!chatHistory[phoneNumber]) {
+          chatHistory[phoneNumber] = [];
+        }
+        chatHistory[phoneNumber].push(message);
+        if (activeChat === phoneNumber) {
+          displayChatHistory(phoneNumber);
+        }
+        updateChatList();
+      }
+    });
+  
+    // Function to display chat history for a specific phone number
+    function displayChatHistory(phoneNumber) {
+      chatMessages.innerHTML = ""; // Clear previous chat messages
+      const history = chatHistory[phoneNumber];
+      if (history && history.length > 0) {
+        history.forEach((message) => {
+          displayMessage(message);
+        });
+      }
+    }
+  
+    // Function to update the chat list in the sidebar
+    function updateChatList() {
+      chatList.innerHTML = "";
+      Object.keys(chatHistory).forEach((phoneNumber) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = phoneNumber;
+        listItem.dataset.chat = phoneNumber;
+        listItem.addEventListener("click", () => {
+          activeChat = phoneNumber;
+          displayChatHistory(phoneNumber);
+        });
+        chatList.appendChild(listItem);
+      });
+    }
+  
+    // Function to display a message in the chat
+    function displayMessage(message) {
+        //console.log("Inside displayMessage()",message);
+        const messageDiv = document.createElement("div");
+        messageDiv.className = "message";
+        if (message.event === "messageSent") {
+          messageDiv.textContent = `${message.message.from}: ${message.message.body}`;
+          if (message.message.from === "You") {
+            messageDiv.classList.add("user-message");
+          }
+        } else if (message.user === "user") {
+          messageDiv.textContent = `${message.from}: ${message.body}`;
+          messageDiv.classList.add("user-message");
+        } else {
+          messageDiv.textContent = "Unknown message type";
+        }
+        chatMessages.appendChild(messageDiv);
+      }
+  
+    // Function to send a message via WebSocket
+    function sendMessage() {
+      // ...
+    }
+  
+    // Send a message when the Send button is clicked
+    sendMessageBtn.addEventListener("click", sendMessage);
+  
+    // Send a message when the Enter key is pressed
+    messageInput.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        sendMessage();
+      }
+    });
+  
+    // Initialize chat list and display the first chat if available
+    updateChatList();
+    if (Object.keys(chatHistory).length > 0) {
+      activeChat = Object.keys(chatHistory)[0];
+      displayChatHistory(activeChat);
+    }
+  });
+  
